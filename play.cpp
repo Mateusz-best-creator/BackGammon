ab79c08cc29f3c1d4ca5fa97688e49534b8af8e7
Mateusz-best-creator <114943990+Mateusz-best-creator@users.noreply.github.com>
2023-12-23 13:26:39 +0100

    Cleaning play_game section (almost)

--- play.cpp
+++ play.cpp
@@ -4,11 +4,71 @@
 #include "interfaces.h"
 #include "board.h"
 
-bool play(Player* player_1, Player* player_2, Board* table_s, int player_index, bool& start_a_new_game)
+bool prepare_game(Player* player_1, Player* player_2, int& player_index, Board* table_s,
+	int& background_color, int& star_x, int& star_y, int& zn, int& zero)
 {
-	textcolor(LIGHTGRAY);
-	// Generate two random numbers and display them
-	int dice1, dice2;
+	if ((player_1->dice1 == 0 && player_1->dice2 == 0 && player_index == 1) ||
+		(player_2->dice1 == 0 && player_2->dice2 == 0 && player_index == 2))
+	{
+		// It means that player cannot make another moves
+		return true;
+	}
+	// Print table
+	print_table(table_s);
+
+	// Print interface for removed pawns
+	print_removed_pawns_interface(player_1, player_2);
+
+	// Set some visual attributes
+	textbackground(background_color);
+
+	// Put our star on the screen
+	gotoxy(star_x, star_y);
+	putch('*');
+	zero = 0;
+	zn = getch();
+	background_color = 0; // Reset background color
+	textbackground(background_color);
+	return false;
+}
+
+void game_loop(Player* player_1, Player* player_2, int& player_index, Board* table_s, int& background_color,
+	int& star_x, int& star_y, int& zero, int& zn, bool& inserting_element, bool& start_a_new_game, char& player_sign)
+{
+	do
+	{
+		if (prepare_game(player_1, player_2, player_index, table_s, background_color, star_x, star_y, zn, zero))
+			break;
+
+		// Determine player pawn sign ("R" or "B")
+		player_sign = player_index == 1 ? player_1->pawn_char : player_2->pawn_char;
+		move(table_s, zn, star_x, star_y, background_color,
+			zero, inserting_element, player_sign, player_1, player_2);
+
+		// After making some move check if the player won the game
+		if (player_index == 1)
+		{
+			if (check_if_player_won(player_1))
+			{
+				start_a_new_game = true;
+				return;
+			}
+		}
+		else
+		{
+			if (check_if_player_won(player_2))
+			{
+				start_a_new_game = true;
+				return;
+			}
+		}
+
+	} while (zn != 'F' && zn != 'Q');
+	clrscr();
+}
+
+void di(Player* player_1, Player* player_2, Board* table_s, int& player_index, int& dice1, int& dice2, char& player_sign)
+{
 	get_random_numbers(&dice1, &dice2);
 	int dice_sum = dice1 + dice2;
 
@@ -23,78 +83,31 @@ bool play(Player* player_1, Player* player_2, Board* table_s, int player_index, 
 		player_2->dice1 = dice1;
 		player_2->dice2 = dice2;
 	}
+	// Print the entire interface for player
+	print_player_interface(player_index, player_1, player_2);
+}
+
+bool play(Player* player_1, Player* player_2, Board* table_s, int player_index, bool& start_a_new_game)
+{
+	textcolor(LIGHTGRAY);
+	// Generate two random numbers and display them
+	int dice1, dice2;
+	char player_sign;
+	di(player_1, player_2, table_s, player_index, dice1, dice2, player_sign);
+
 
 	int zn = 0, background_color = 0, zero = 0;
 
 	// Initialize start coordinates for our '*' pointer, roughly in the middle of the board
 	int star_x = 55, star_y = 16;
 
-	// Print the entire interface for player
-	print_player_interface(player_index, player_1, player_2);
-
 	// Print the top interface
 	print_top_interface(player_1, player_2, true);
 
 	bool inserting_element = false;
 
-	do
-	{
-		if ((player_1->dice1 == 0 && player_1->dice2 == 0 && player_index == 1) ||
-			(player_2->dice1 == 0 && player_2->dice2 == 0 && player_index == 2))
-		{
-			// It means that player cannot make another moves
-			break;
-		}
-		
-		// Determine player pawn sign ("R" or "B")
-		char player_sign = player_index == 1 ? player_1->pawn_char : player_2->pawn_char;
-
-		// Print table
-		print_table(table_s);
-
-		// Print interface for removed pawns
-		print_removed_pawns_interface(player_1, player_2);
-
-		// Set some visual attributes
-		textbackground(background_color);
-
-		// Put our star on the screen
-		gotoxy(star_x, star_y);
-		putch('*');
-
-		// we wait for key press and get its code
-		// most key codes correspond to the characters, like
-		// a is 'a', 2 is '2', + is '+', but some special keys
-		// like cursors provide two characters, where the first
-		// one is zero, e.g., "up arrow" is zero and 'H'
-		zero = 0;
-		zn = getch();
-		background_color = 0; // Reset background color
-		textbackground(background_color);
-
-		move(table_s, zn, star_x, star_y, background_color,
-			zero, inserting_element, player_sign, player_1, player_2);
-
-		// After making some move check if the player won the game
-		if (player_index == 1)
-		{
-			if (check_if_player_won(player_1))
-			{
-				start_a_new_game = true;
-				return true;
-			}
-		}
-		else
-		{
-			if (check_if_player_won(player_2))
-			{
-				start_a_new_game = true;
-				return true;
-			}
-		}
-
-	} while (zn != 'F' && zn != 'Q');
-	clrscr();
+	game_loop(player_1, player_2, player_index, table_s, background_color, star_x, star_y,
+		zero, zn, inserting_element, start_a_new_game, player_sign);
 
 	// Stop playing the game
 	if (zn == 'Q') return true;
--- start_game.cpp
+++ start_game.cpp
@@ -11,181 +11,219 @@
 #include "interfaces.h"
 #include "board.h"
 
-void start_game(PlayersDatabase* database, Player* player_1, Player* player_2, bool& read_from_file)
+void AI_both(Player* player_1, Player* player_2, Board* table, bool& first_time, int& player_index)
+{
+	// Show the board for 2 seconds in case AI vs AI
+	if (player_1->name == "AI1" && player_2->name == "AI2")
+	{
+		print_table(table);
+		print_removed_pawns_interface(player_1, player_2);
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
+	clrscr();
+
+	// Print who start the game
+	if (first_time)
+	{
+		first_time = false;
+		print_which_player_begins(player_index);
+	}
+}
+
+bool AI2(Player* player_1, Player* player_2, PlayersDatabase* database, Board* table, int& player_index)
+{
+	AI2_make_move(table, player_2);
+	if (check_if_player_won(player_2))
+	{
+		// Clear the removed pawns array
+		for (size_t i = 0; i < NUMBER_OF_PAWNS_FOR_ONE_PLAYER; i++)
+		{
+			player_2->removed_pawns[i] = 'E';
+		}
+		player_2->number_of_removed_pawns = 0;
+		database_functionality(database);
+		return true;
+	}
+	if (player_index == 1) { player_index = 2; }
+	else { player_index = 1; }
+	// Print state of the game to the file
+	write_to_file_visually(table->table, player_1, player_2);
+	write_to_file(table, player_1, player_2);
+	return false;
+}
+
+bool AI1(Player* player_1, Player* player_2, PlayersDatabase* database, Board* table, int& player_index)
+{
+	AI1_make_move(table, player_1);
+	if (check_if_player_won(player_1))
+	{
+		// Clear the removed pawns array
+		for (size_t i = 0; i < NUMBER_OF_PAWNS_FOR_ONE_PLAYER; i++)
+		{
+			player_1->removed_pawns[i] = 'E';
+		}
+		player_1->number_of_removed_pawns = 0;
+		database_functionality(database);
+		return true;
+	}
+	if (player_index == 1) { player_index = 2; }
+	else { player_index = 1; }
+	// Print state of the game to the file
+	write_to_file_visually(table->table, player_1, player_2);
+	write_to_file(table, player_1, player_2);
+	return false;
+}
+
+void P_case(Player* p1, Player* p2, PlayersDatabase* db, Board* t, int& pi, bool& ft,
+	bool& sp, bool& sng)
+{
+	// If player typed "P" we will start a game in a loop
+	do
+	{
+		AI_both(p1, p2, t, ft, pi);
+
+		// First AI bot makes a move
+		if (pi == 1 && p1->name == "AI1")
+		{
+			if (AI1(p1, p2, db, t, pi))
+				return;
+			else
+				continue;
+		}
+
+		// Second IA bot makes a move
+		if (pi == 2 && p2->name == "AI2")
+		{
+			if (AI2(p1, p2, db, t, pi))
+				return;
+			else
+				continue;
+		}
+
+		sp = play(p1, p2, t, pi, sng);
+		// Change player_index
+		if (pi == 1) { pi = 2; }
+		else { pi = 1; }
+
+		if (!sp)
+		{
+			// Print state of the game to the file
+			write_to_file_visually(t->table, p1, p2);
+			write_to_file(t, p1, p2);
+		}
+	} while (!sp);
+}
+
+bool end(Board* t,bool& sng, PlayersDatabase* dt, Player* p1, Player* p2, bool& rf, bool& gdt)
+{
+	_setcursortype(_NORMALCURSOR);	// show the cursor so it will be visible after the program ends
+
+	// After the game free the memory to avoid memory leaks
+	delete t;
+
+	// After everything check if we want to start a new game
+	if (sng)
+	{
+		start_game(dt, p1, p2, rf);
+		return true;
+	}
+
+	if (gdt)
+	{
+		database_functionality(dt);
+		return true;
+	}
+
+	if (!sng)
+	{
+		// After the game free the memory
+		//delete[] database;
+		return true;
+	}
+	return false;
+}
+
+Board* pre(PlayersDatabase* d, Player* p1, Player* p2, bool& rf)
 {
 	// We need to do some preprocessing before we start a game
-	player_1->pawn_char = 'B';
-	player_2->pawn_char = 'R';
-	player_1->player_index = 1;
-	player_2->player_index = 2;
-
-	int zn = 0;
-
-	// This variable will indicate if user entered [P] to play for the first time
-	bool first_time = true;
+	p1->pawn_char = 'B';
+	p2->pawn_char = 'R';
+	p1->player_index = 1;
+	p2->player_index = 2;
 
 	// Initialize the board with default pawn places
-	Board* table = new Board();
+	Board* t = new Board();
 
 	// In case we read from file
-	if (read_from_file)
+	if (rf)
 	{
-		table = load_table_from_file(player_1, player_2);
+		t = load_table_from_file(p1, p2);
 	}
 	else
 	{
-		initialize_table(table);
+		initialize_table(t);
 	}
-	read_from_file = false;
-	print_table(table);
+	rf = false;
+	print_table(t);
 	// This index will keep track which player now makes a move
-	int player_index = 1;
 
 	// Print initial state of the game
-	write_to_file_visually(table->table, player_1, player_2);
-	write_to_file(table, player_1, player_2);
+	write_to_file_visually(t->table, p1, p2);
+	write_to_file(t, p1, p2);
+	return t;
+}
+
+void start_game(PlayersDatabase* d, Player* p1, Player* p2, bool& rf)
+{
+	Board* t = pre(d, p1, p2, rf);
+	int player_index = 1;
+	int zn = 0;
+
+	// This variable will indicate if user entered [P] to play for the first time
+	bool fst = true;
 
 	// This boolean value will indicate if we want to stop the game
-	bool stop_playing = false;
+	bool stp = false;
 
 	// This value will indicate if we want to start a new game
-	bool start_a_new_game = false;
+	bool sng = false;
 
 	// This boolean value will indicate whether or not we want to go to our database interface
-	bool go_to_database;
+	bool gtd;
 
 	do
 	{
-		print_table(table);
-		print_top_interface(player_1, player_2, false);
+		print_table(t);
+		print_top_interface(p1, p2, false);
 		zn = getch();
 
 		if (zn == 'C')
 		{
-			table->color++;
-			if (table->color > 15)
+			t->color++;
+			if (t->color > 15)
 			{
-				table->color = 0;
+				t->color = 0;
 			}
 		}
 		else if (zn == 'P')
 		{
-			// If player typed "P" we will start a game in a loop
-			do
-			{
-				// Show the board for 2 seconds in case AI vs AI
-				if (player_1->name == "AI1" && player_2->name == "AI2")
-				{
-					print_table(table);
-					print_removed_pawns_interface(player_1, player_2);
-					std::this_thread::sleep_for(std::chrono::seconds(1));
-				}
-				clrscr();
-
-				// Print who start the game
-				if (first_time)
-				{
-					first_time = false;
-					print_which_player_begins(player_index);
-				}
-				
-				// First AI bot makes a move
-				if (player_index == 1 && player_1->name == "AI1")
-				{
-					AI1_make_move(table, player_1);
-					if (check_if_player_won(player_1))
-					{
-						// Clear the removed pawns array
-						for (size_t i = 0; i < NUMBER_OF_PAWNS_FOR_ONE_PLAYER; i++)
-						{
-							player_1->removed_pawns[i] = 'E';
-						}
-						player_1->number_of_removed_pawns = 0;
-						database_functionality(database);
-						return;
-					}
-					if (player_index == 1) { player_index = 2; }
-					else { player_index = 1; }
-					// Print state of the game to the file
-					write_to_file_visually(table->table, player_1, player_2);
-					write_to_file(table, player_1, player_2);
-					continue;
-				}
-
-				// Second IA bot makes a move
-				if (player_index == 2 && player_2->name == "AI2")
-				{
-					AI2_make_move(table, player_2);
-					if (check_if_player_won(player_2))
-					{
-						// Clear the removed pawns array
-						for (size_t i = 0; i < NUMBER_OF_PAWNS_FOR_ONE_PLAYER; i++)
-						{
-							player_2->removed_pawns[i] = 'E';
-						}
-						player_2->number_of_removed_pawns = 0;
-						database_functionality(database);
-						return;
-					}
-					if (player_index == 1) { player_index = 2; }
-					else { player_index = 1; }
-					// Print state of the game to the file
-					write_to_file_visually(table->table, player_1, player_2);
-					write_to_file(table, player_1, player_2);
-					continue;
-				}
-
-				stop_playing = play(player_1, player_2, table, player_index, start_a_new_game);
-				// Change player_index
-				if (player_index == 1) { player_index = 2; }
-				else { player_index = 1; }
-
-				if (!stop_playing)
-				{
-					// Print state of the game to the file
-					write_to_file_visually(table->table, player_1, player_2);
-					write_to_file(table, player_1, player_2);
-				}
-			} while (!stop_playing);
+			P_case(p1, p2, d, t, player_index, fst, stp, sng);
 		}
 
 		else if (zn == 'D')
 		{
-			go_to_database = true;
+			gtd = true;
 			break;
 		}
 
-		if (start_a_new_game)
+		if (sng)
 		{
 			clrscr();
 			break;
 		}
-		go_to_database = false;
+		gtd = false;
 	} while (zn != 'Q' && zn != 'q');
 
-	_setcursortype(_NORMALCURSOR);	// show the cursor so it will be visible after the program ends
-
-	// After the game free the memory to avoid memory leaks
-	delete table;
-
-
-	// After everything check if we want to start a new game
-	if (start_a_new_game)
-	{
-		start_game(database, player_1, player_2, read_from_file);
+	if (end(t, sng, d, p1, p2, rf, gtd))
 		return;
-	}
-
-	if (go_to_database)
-	{
-		database_functionality(database);
-		return;
-	}
-
-	if (!start_a_new_game)
-	{
-		// After the game free the memory
-		//delete[] database;
-		return;
-	}
 }
